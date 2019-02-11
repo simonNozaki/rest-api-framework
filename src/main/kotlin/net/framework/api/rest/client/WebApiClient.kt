e25c0dce3699a6473bb000fe0583ef2b57ddc180
simonNozaki <tldk1993@gmail.com>
2019-02-12 08:50:36 +0900

    Kotlin対応、クライアントクラス新設

--- src/main/kotlin/net/framework/api/rest/client/WebApiClient.kt
+++ src/main/kotlin/net/framework/api/rest/client/WebApiClient.kt
@@ -1,12 +1,37 @@
 package net.framework.api.rest.client
 
+import javax.ws.rs.client.Client
+import javax.ws.rs.client.ClientBuilder
+import javax.ws.rs.client.WebTarget
+import javax.ws.rs.core.MultivaluedMap
+
 /**
  * Web APIクライアントクラス。
  */
-class WebApiClient {
+class WebApiClient(uri: String, path: String, headerMap: HashMap<String, String>) {
 
     /**
-     * APIクライアントインスタンス
+     * クラスの初期化
      */
+    init {
+        /**
+         * APIクライアントインスタンス
+         */
+        val client: Client = ClientBuilder.newClient();
+
+        /**
+         * API接続先設定
+         */
+        var webTarget: WebTarget = client.target(uri).path(path);
+
+        /**
+         * ヘッダー指定
+         */
+        var headers: MultivaluedMap<String, Object>;
+        headerMap.map { entry -> {
+//            headers.putSingle(entry.key, (entry.value);
+        } }
+    }
+
 
 }
